9313865cfcd7b3b0d31ad897310fbaea9dfec755
M.Sakina <sakhinamammadzade0406@gmail.com>
2024-02-25 02:52:40 +0400

    Added MovieServices

--- CleanMovie.Application/IMovieService.cs
+++ CleanMovie.Application/IMovieService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanMovie.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,8 @@ namespace CleanMovie.Application
 {
     public interface IMovieService
     {
+        List<Movie> GetAllMovies();
+        Movie CreateMovie(Movie movie);
+
     }
 }
--- CleanMovie.Application/MovieService.cs
+++ CleanMovie.Application/MovieService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CleanMovie.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,42 @@ using System.Threading.Tasks;
 
 namespace CleanMovie.Application
 {
-    public class MovieService
+    public class MovieService : IMovieService
     {
+        private readonly IMovieRepository _movieRepository;
+
+        //Constructor Dependency Injection
+        public MovieService(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+
+        public Movie CreateMovie(Movie movie)
+        {
+            try
+            {
+                _movieRepository.CreateMovie(movie);
+                return movie;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public List<Movie> GetAllMovies()
+        {
+            try
+            {
+                var movies = _movieRepository.GetAllMovies();
+                return movies;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
     }
 }
