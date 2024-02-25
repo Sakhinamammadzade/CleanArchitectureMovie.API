9bac120db5d3d5deb266c044854e8a305ebe81fa
M.Sakina <sakhinamammadzade0406@gmail.com>
2024-02-25 17:32:08 +0400

    MovieController added

---
+++ API/Controllers/MovieController.cs
@@ -0,0 +1,32 @@
+﻿using CleanMovie.Application;
+using CleanMovie.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanMovie.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private readonly IMovieService _movieService;
+
+        public MovieController(IMovieService movieService)
+        {
+            _movieService = movieService;
+        }
+        [HttpGet]
+        public ActionResult<List<Movie>> Get()
+        {
+            var movies = _movieService.GetAllMovies();
+            return movies;
+
+        }
+        [HttpPost]
+        public ActionResult<Movie> PostMovie(Movie movie)
+        {
+            var movies = _movieService.CreateMovie(movie);
+            return movies;
+        }
+    }
+}
