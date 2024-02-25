1b41435be4f47f2d8ff5f88f9ae685f9ecb72e44
M.Sakina <sakhinamammadzade0406@gmail.com>
2024-02-25 17:41:43 +0400

    Movie Repository added bug fix

--- CleanMovie.Infrastructure/MovieRepository.cs
+++ CleanMovie.Infrastructure/MovieRepository.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using CleanMovie.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace CleanMovie.Infrastructure
 {
-    public class MovieRepository
+    public class MovieRepository: IMovieRepository
     {
+
+        private readonly MovieDbContext _movieDbContext;
+
+        public MovieRepository(MovieDbContext movieDbContext)
+        {
+            _movieDbContext = movieDbContext;
+        }
+        public Movie CreateMovie(Movie movie)
+        {
+            _movieDbContext.Movies.Add(movie);
+            _movieDbContext.SaveChanges();
+
+            return movie;
+        }
+
+        public List<Movie> GetAllMovies()
+        {
+            return _movieDbContext.Movies.ToList();
+        }
+    }
+
+    public interface IMovieRepository
+    {
+        List<Movie> GetAllMovies();
+        Movie CreateMovie(Movie movie);
     }
 }
